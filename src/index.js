66a6d9eefc966818f0695afcdd1e70e40fb74dc5
Carbene Hu <hyikerhu0212@gmail.com>
2025-06-20 14:15:10 +0800

    Fix reservation cancellation.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@ant-design/v5-patch-for-react-19';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ConfigProvider } from 'antd';
+import { ConfigProvider, unstableSetRender } from 'antd';
+
+unstableSetRender((node, container) => {
+  container._reactRoot ||= ReactDOM.createRoot(container);
+  const root = container._reactRoot;
+  root.render(node);
+  return async () => {
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    root.unmount();
+  };
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
